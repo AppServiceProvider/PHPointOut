debfc1b9796fc13f304f94737cd547e895f44859
romanofficial <roman.shaon@gmail.com>
2024-09-15 00:25:24 +0600

    autoload spl_autoload_register

--- autoloading/main.php
+++ autoloading/main.php
@@ -1,7 +1,15 @@
 <?php 
 
-include "bike.php";
-include "planet.php";
-include "spaceship.php";
+// include "bike.php";
+// include "planet.php";
+// include "spaceship.php";
 
-(new Spaceship)->lunch();
+
+
+function autoload($name) {
+    include strtolower("{$name}.php");  // Convert class name to lowercase and include the file
+}
+// Register the autoload function
+spl_autoload_register('autoload');
+
+(new Spaceship)->lunch(); // Create a new object of class Spaceship (should trigger the autoload)
