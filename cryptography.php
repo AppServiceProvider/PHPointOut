c69226557cf6dcb76e0a16c82d5a07ae28c33b54
appserviceprovider <roman.shaon@gmail.com>
2024-09-22 02:05:13 +0600

    hashing

--- cryptography.php
+++ cryptography.php
@@ -1,1 +1,15 @@
 <?php 
+
+// echo md5('hellow world');
+// echo sha1('hellow world');
+
+// print_r(hash_algos());
+
+// echo hash("md5","hellow world");
+// echo hash("sha1","hellow world");
+
+// echo bin2hex(mhash(MHASH_MD5,"HELLOW WORLD")); 
+
+echo bin2hex(mhash(MHASH_SHA512,"HELLOW WORLD")); 
+
+
