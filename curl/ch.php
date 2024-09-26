7baf4680eb3338ddf080668173afaf38511d6df2
appserviceprovider <roman.shaon@gmail.com>
2024-09-26 15:55:58 +0600

    post request curl

--- curl/ch.php
+++ curl/ch.php
@@ -2,4 +2,7 @@
 
 
 $ch =curl_init('http://localhost:3000/curl/hellow.php');
+
+curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'secret=noodles');
 curl_exec($ch);  // http request data print
--- curl/hellow.php
+++ curl/hellow.php
@@ -2,4 +2,8 @@
 /*
 http://localhost:3000/curl/hellow.php 
 */
-echo "hellow world. this is curl";
+echo "hellow world. this is curl \n";
+
+if( isset($_POST["secret"]) && "noodles" == $_POST["secret"]){ //if post is secret and value is noodles
+    echo "post secret works \n";
+}
