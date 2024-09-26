4bc2178c5e92d4ea1282df9eec49267023e47ef0
appserviceprovider <roman.shaon@gmail.com>
2024-09-26 17:50:16 +0600

    modify value print http request

--- curl/ch.php
+++ curl/ch.php
@@ -1,8 +1,13 @@
 <?php 
 
-
 $ch =curl_init('http://localhost:3000/curl/hellow.php');
 
 curl_setopt($ch,CURLOPT_CUSTOMREQUEST,"POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, 'secret=noodles');
-curl_exec($ch);  // http request data print
+// curl_exec($ch);  // http request data print
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$var = curl_exec($ch);
+echo strtoupper($var);
+
