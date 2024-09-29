802fd8d076da2f9dd1b58329063eceecb0659511
appserviceprovider <roman.shaon@gmail.com>
2024-09-29 13:28:31 +0600

    object cast object

--- castingNullBooleanOtherObj.php
+++ castingNullBooleanOtherObj.php
@@ -7,3 +7,19 @@ $object = (object) $nullValue;
 
 $bool = true;
 $object = (object) $bool;
+
+/*
+If you cast an object of a custom class to (object), nothing changes—it remains an object of the original class.
+i dont understand what diff object and cast object
+*/
+class MyClass {
+    public $property = 'value';
+}
+
+$myObject = new MyClass();
+$object = (object) $myObject;
+
+
+var_dump($myObject);
+var_dump($object);
+
