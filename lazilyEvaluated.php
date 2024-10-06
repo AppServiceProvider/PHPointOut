c13bb7893d0d5b33c91e979bc80c069b04c1bcc9
Roman Hossain Shaon <roman.shaon@gmail.com>
2024-10-07 03:17:05 +0600

    method chaining array_merge

--- lazilyEvaluated.php
+++ lazilyEvaluated.php
@@ -16,3 +16,33 @@ class Lazy {
 
 $lazy = new Lazy();
 echo $lazy->addOperation('op1')->addOperation('op2')->execute();
+
+
+
+
+//================================================
+$cars = array ( //two-dimensional array
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+  );
+  
+  print_r(array_merge(...$cars));
+//===========
+class Lazy1 {
+    protected $operations = [];
+    public function addOperation(...$operation) {
+        // Merge the operations array into $this->operations
+        $this->operations = array_merge($this->operations, $operation); //array_merge(rq array1, array2,array3...)
+        return $this; // Return current object for chaining
+    }
+    public function execute() {
+        foreach ($this->operations as $operation) {
+            echo $operation;
+        }
+        return ""; // Return an empty string to avoid issues with echo
+    }
+}
+$lazy = new Lazy1();
+echo $lazy->addOperation('op1', 'op2', 'op3')->execute();
