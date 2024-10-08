fef92c7476e1fd5aad9230d42723076189750103
Roman Hossain Shaon <roman.shaon@gmail.com>
2024-10-09 00:41:15 +0600

    validate empty is_string trim

--- arrayPositionalAgrument.php
+++ arrayPositionalAgrument.php
@@ -3,17 +3,21 @@
 class Demo{
    private $propertyVar=[];
    function demofun(...$para){
-    if(empty($para)){
+    if (empty($para)) {
         throw new InvalidArgumentException("No arguments provided");
     }
     foreach ($para as $value) {
         if (!is_string($value)) {
-            throw new InvalidArgumentException("Only integers are allowed.");
+            throw new InvalidArgumentException("Only strings are allowed.");
         }
-        $this->propertyVar[]=$value;
+        if (trim($value) === '') { // Check for empty string
+            throw new InvalidArgumentException("Empty strings are not allowed.");
         }
-        return $this;
+        $this->propertyVar[] = $value;
+    }
+    return $this;
    }
+
    function exMethod(){
     foreach ($this->propertyVar as $vale){
         echo $vale;
@@ -22,8 +26,10 @@ class Demo{
 }
 
 try {
-    (new Demo())->demofun(12)
+    (new Demo())->demofun('')
     ->exMethod();
 } catch (InvalidArgumentException $e) {
     echo 'Exception: ' . $e->getMessage() . PHP_EOL;
+}catch (Exception $e) {
+    echo "Caught exception: " . $e->getMessage();
 }
