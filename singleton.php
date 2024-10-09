b2570ad30197fe598f4a5d9502ab7c7161672ded
Roman Hossain Shaon <roman.shaon@gmail.com>
2024-10-09 20:39:28 +0600

    basic singleton

--- singleton.php
+++ singleton.php
@@ -6,7 +6,7 @@ This is useful when exactly one object is needed to coordinate actions across th
 class MyClass2 {
     private static $instance = null;
     private function __construct() {
-        echo "hellow".PHP_EOL;
+        echo "hellow ";
     }
     public static function getInstance() {
         if (self::$instance === null) {
@@ -23,8 +23,5 @@ $instance2 = MyClass2::getInstance();
 $instance3 = MyClass2::getInstance();
 
 $instance->someMethod(); // hellow Executing some method
-$instance->someMethod(); // Executing some method
-$instance->someMethod(); // Executing some method
-
-$instance2->someMethod(); // Executing some method.
-$instance3->someMethod(); // Executing some method
+$instance2->someMethod(); // Executing some method
+$instance3->someMethod(); // Executing some metho.
